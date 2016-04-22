9843482948cc975e2deaa7447b02a38d8d4a2804
HariniParth <parth.harini@gmail.com>
2016-04-22 15:27:27 -0700

    Initial

---
+++ kernel/mem.c
@@ -0,0 +1,29 @@
+
+#include <kernel.h>
+
+
+void poke_b (MEM_ADDR addr, BYTE value)
+{
+}
+
+void poke_w (MEM_ADDR addr, WORD value)
+{
+}
+
+void poke_l (MEM_ADDR addr, LONG value)
+{
+}
+
+BYTE peek_b (MEM_ADDR addr)
+{
+}
+
+WORD peek_w (MEM_ADDR addr)
+{
+}
+
+LONG peek_l (MEM_ADDR addr)
+{
+}
+
+
---
+++ kernel/stdlib.c
@@ -0,0 +1,16 @@
+
+#include <kernel.h>
+
+
+int k_strlen(const char* str)
+{
+}
+
+void* k_memcpy(void* dst, const void* src, int len)
+{
+}
+
+int k_memcmp(const void* b1, const void* b2, int len)
+{
+}
+
